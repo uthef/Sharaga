cbd473c5e7433bed4e63eec6d6914fdaaac7c195
Opleyha <google@mail.ru>
2022-10-11 09:53:37 +0300

    забубенил Task 7

---
+++ UI-PR-TASK7/TaskTwoWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_PR_TASK7
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskTwoWindow.xaml
+    /// </summary>
+    public partial class TaskTwoWindow : Window
+    {
+        public TaskTwoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
